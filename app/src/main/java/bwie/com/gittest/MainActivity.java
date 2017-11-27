c86b286ad38346455af8328589529bc78b5f7aa6
songcongcong <songcong102912@qq.com>
2017-11-27 15:14:04 +0800

    'dd'

--- app/src/main/java/bwie/com/gittest/MainActivity.java
+++ app/src/main/java/bwie/com/gittest/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //第一次提交
+
         //状态
     }
 }
